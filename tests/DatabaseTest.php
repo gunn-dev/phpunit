a220142a7e57d9beaf802e52e89139b9fa534643
Ben Gunn <bengunn.dev@gmail.com>
2022-04-30 00:19:05 +0630

    update

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class DatabaseTest extends TestCase
+{
+
+    private static $dbh;
+
+    // for performance, connect db before class start and end after class
+    public static function setUpBeforeClass(): void
+    {
+        self::$dbh = new PDO('sqlite::memory:');
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$dbh = null;
+    }
+}
